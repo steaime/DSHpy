79599da9e2e1381046cf748392ed80b32cae4ed4
steaime <63365480+steaime@users.noreply.github.com>
2020-05-28 00:21:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='DSH',
       version='0.0.1',
       description='Analyze DSH videos',
-      url='https://github.com/phys201/optical_lattice',
+      url='https://github.com/steaime/DSHpy',
       author='Stefano Aime, Matteo Sabato',
       author_email='aime@seas.harvard.edu',
       license='GNU GPL',
